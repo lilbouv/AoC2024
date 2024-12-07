a5b51141371c0bae04fa538f5c02429e2f36f253
zbouvier <thebouvse@gmail.com>
2024-12-07 13:25:24 -0500

    day 7

--- day6/day6.py
+++ day6/day6.py
@@ -5,6 +5,8 @@ f = [x.strip() for x in open("day6.txt", "r").readlines()]
 # Read the file and split the input into groups
 # Read into 2d array
 #print(f)
+import copy
+
 def getGuardPosition():
     for row in range(len(f)):
         for character in range(len(f[row])):
@@ -24,7 +26,7 @@ def calculateAllGuardSpots(x,y, f=f):
 
     # Check last move in file and if it is not a #, move the guard position
     pastGuardPositions = [(guardPosition, LAST_MOVE)]
-    while guardPosition[0] > 0 and guardPosition[1] > 0 and guardPosition[0] < len(f) and guardPosition[1] < len(f[0]):
+    while guardPosition[0] >= 0 and guardPosition[1] >= 0 and guardPosition[0] < len(f) and guardPosition[1] < len(f[0]):
         #if it reaches a previous guard position in the same direction, then it will loop infinitely.
         
         try:
@@ -34,21 +36,8 @@ def calculateAllGuardSpots(x,y, f=f):
                     return []
                 pastGuardPositions.append((guardPosition, LAST_MOVE))
             else:
+                
                 LAST_MOVE = (LAST_MOVE + 1) % 4
-                guardPosition = (guardPosition[0] + DIRECTIONS[LAST_MOVE][0], guardPosition[1] + DIRECTIONS[LAST_MOVE][1])
-                if (guardPosition, LAST_MOVE) in pastGuardPositions:
-                    return []
-                pastGuardPositions.append((guardPosition, LAST_MOVE))
-            
-            #print a recreation of the map with the guard position being updated
-            # for row in range(len(f)):
-            #     for character in range(len(f[row])):
-            #         pass
-                    # if (row, character) == guardPosition:
-                    #     print("^", end="")
-                    # else:
-                    #     print(f[row][character].replace("^", "."), end="")
-                #print()
         except:
             break
     #print distinct guard positions
@@ -57,6 +46,7 @@ def calculateAllGuardSpots(x,y, f=f):
     #Next, find the number of distinct places I can place an obstacle to cause the guard to infinitely loop.
     #Check all guard positions and see if placing a wall will cause the guard to loop infinitely
 startingPos = getGuardPosition()
+print(startingPos)
 previousGuardSpots = calculateAllGuardSpots(startingPos[0], startingPos[1], f)
 print("-----------------")
 c = 0
@@ -66,7 +56,7 @@ check = 0
 for spot in previousGuardSpots:
     print("Checking spot", str(check), spot)
     check+=1
-    f_new = f.copy()
+    f_new = copy.deepcopy(f)
     f_new[spot[0][0]] = f_new[spot[0][0]][:spot[0][1]] + "#" + f_new[spot[0][0]][spot[0][1]+1:]
 
     startingPos = getGuardPosition()
@@ -74,5 +64,11 @@ for spot in previousGuardSpots:
     if len(previousGuardSpots) == 0:
         c_spot.append(spot)
         c+=1
-print(len(list(set([x[0] for x in c_spot]))))
+#remove any from the starting guard position and any that have a negative index
+startingPos = getGuardPosition()
+if startingPos in c_spot[0]:
+    c_spot.remove(startingPos)
+if c_spot[0][0]:
+    c_spot.remove((startingPos[0] - 1, startingPos[1]))
 
+print(len(set([x[0] for x in c_spot])))
--- day7/day7.py
+++ day7/day7.py
@@ -1,10 +1,57 @@
-# Path: day7/day7.py
-# Author: thebo
-# Date: 2024-12-01 10:13:47.847310
-#
-# Problem:
-#
-# Solution:
-#
-#
-# Part 2
+f = open("day7.txt", "r")
+def evaluate_left_to_right(equation):
+    # Split the equation into tokens (numbers and operators)
+    tokens = equation.split()
+    
+    # Start with the first number
+    result = int(tokens[0])
+    
+    # Iterate through the tokens in pairs of (operator, operand)
+    for i in range(1, len(tokens), 2):
+        operator = tokens[i]
+        operand = int(tokens[i + 1])
+        
+        if operator == '+':
+            result += operand
+        elif operator == '*':
+            result *= operand
+        elif operator == "||":
+            result = int(str(result) + str(operand))
+        else:
+            raise ValueError(f"Unsupported operator: {operator}")
+    
+    return int(result)
+from itertools import product
+def generate_equations(numbers):
+    if len(numbers) < 2:
+        raise ValueError("The list of integers must contain at least two numbers.")
+    
+    # Get all combinations of operators for the number of gaps between numbers
+    operators = ['+', '*', "||"]
+    operator_combinations = product(operators, repeat=len(numbers) - 1)
+    
+    # Generate equations by interleaving numbers with operator combinations
+    equations = []
+    for ops in operator_combinations:
+        equation = str(numbers[0])
+        for num, op in zip(numbers[1:], ops):
+            equation += f" {op} {num}"
+        equations.append(equation)
+    
+    return equations
+
+lines = f.readlines()
+final_total = 0
+for line in lines:
+    total = line.split(":")[0]
+    numbers = [int(x) for x in line.split(":")[1].strip().split(" ")]
+    for equation in generate_equations(numbers):
+        #print(evaluate_left_to_right(equation), total)
+        if evaluate_left_to_right(equation) == int(total):
+            #print("HIT")
+            final_total += int(total)
+            break
+    #print("-------")
+
+    
+print(final_total)
